6fa90c52f9f2ce62ff12f23c2eaafd63c30e488b
Aditya Ashvin <53205379+AdityaAshvin@users.noreply.github.com>
2019-12-29 19:51:15 +0530

    Update Admin_Registration_and_Login_page.php

--- Admin_Registration_and_Login_page.php
+++ Admin_Registration_and_Login_page.php
@@ -24,4 +24,24 @@
              </div>
              <button type="submit" class="btn btn-primary"> Login </button>
          </form>
+     </div>
+
+<div class="col-md-6 login-right">
+         <h2> Register Here </h2>
+         <form action="admin_registration.php" method="post">
+          <div class="form-group">
+             <label>Username</label>
+             <input type="text" name="user" class="form-control" required>
+             </div>
+          <div class="form-group">
+              <label>email-id</label>
+             <input type="email" name="email" class="form-control" required>
+             </div>
+          <div class="form-group">
+              <label>password</label>
+             <input type="password" name="password" class="form-control" required>
+             </div>
+             <button type="submit" class="btn btn-primary"> Register </button>
+</form>
+</div>
 </body>
